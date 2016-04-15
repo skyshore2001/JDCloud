0286370dddcedd4afe5b6a496d19c68c29b6fc81
liangjian <liangjian@oliveche.com>
2016-04-15 19:44:21 +0800

    bugfix: apache中pathinfo为null

--- product/server/index.php
+++ product/server/index.php
@@ -3,7 +3,7 @@
 $DEF_PAGE = "m2/index.html";
 
 $pathInfo = $_SERVER["PATH_INFO"];
-if ($pathInfo === '/')
+if (!isset($pathInfo) || $pathInfo === '/')
 	header("Location: $DEF_PAGE");
 else
 	include('api.php');
