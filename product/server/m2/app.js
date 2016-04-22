5bca84ac222e89d160d7bcc769917630fc3be0c0
liangjian <liangjian@oliveche.com>
2016-04-23 01:58:23 +0800

    上下拉列表更新: initNavbarAndList增强，支持单列表，或button组与list组对应

--- product/server/m2/app.js
+++ product/server/m2/app.js
@@ -3,7 +3,7 @@
 
 // ====== app fw {{{
 /**
-@fn initPullList(obj, opt)
+@fn initPullList(container, opt)
 
 为列表添加下拉刷新和上拉加载功能。
 
@@ -39,19 +39,31 @@
 			// ....
 			if (data.nextkey)
 				nextkey = data.nextkey;
+			// TODO: 处理分页结束即nextkey为空的情况。
 		});
 	}
 
 注意：
 
 - 由于page body的高度自动由框架设定，所以可以作为带滚动条的容器；如果是其它容器，一定要确保它有限定的宽度，以便可以必要时出现滚动条。
+- *** 由于处理分页的逻辑比较复杂，请调用 initNavbarAndList替代, 即使只有一个list；它会屏蔽nextkey, refresh等细节，并做一些优化。像这样调用：
 
-@param obj 容器，它的高度应该是限定的，因而当内部内容过长时才可出现滚动条
-@param opt {onLoadItem, autoLoadMore?=true, threshold?=100, onHint?}
+	initNavbarAndList(jpage, {
+		pageItf: PageOrders,
+		navRef: null,
+		listRef: jlst,
+		onGetQueryParam: ...
+		onAddItem: ...
+	});
+
+本函数参数如下：
+
+@param container 容器，它的高度应该是限定的，因而当内部内容过长时才可出现滚动条
+@param opt {onLoadItem, autoLoadMore?=true, threshold?=80, onHint?}
 
 @param onLoadItem function(isRefresh)
 
-在合适的时机，它调用 onLoadItem(true) 来刷新列表，调用 onLoadItem(false) 来加载列表的下一页。在该回调中this为obj对象（即容器）。实现该函数时应当自行管理当前的页号(pagekey)
+在合适的时机，它调用 onLoadItem(true) 来刷新列表，调用 onLoadItem(false) 来加载列表的下一页。在该回调中this为container对象（即容器）。实现该函数时应当自行管理当前的页号(pagekey)
 
 @param autoLoadMore 当滑动到页面下方时（距离底部TRIGGER_AUTOLOAD=30px以内）自动加载更多项目。
 
@@ -67,10 +79,17 @@
 提供提示用户刷新或加载的动画效果. 缺省实现是下拉或上拉时显示提示信息。
 
 */
-function initPullList(obj, opt)
+function initPullList(container, opt)
 {
-	var m_touchev = null; // {ac, x0, y0}
-	var m_mouseMoved = false;
+	var opt_ = $.extend({
+		threshold: 80,
+		onHint: onHint,
+		autoLoadMore: true,
+	}, opt);
+	var cont_ = container;
+
+	var touchev_ = null; // {ac, x0, y0}
+	var mouseMoved_ = false;
 	var SAMPLE_INTERVAL = 200; // ms
 	var TRIGGER_AUTOLOAD = 30; // px
 
@@ -78,22 +97,16 @@ function initPullList(obj, opt)
 		setTimeout(fn, 1000/60);
 	};
 
-	if (opt.threshold == null)
-		opt.threshold = 100;
-	if (opt.onHint == null)
-		opt.onHint = onHint;
-	if (opt.autoLoadMore == null)
-		opt.autoLoadMore = true;
-
 	if ("ontouchstart" in window) {
-		obj.addEventListener("touchstart", touchStart);
-		obj.addEventListener("touchmove", touchMove);
-		obj.addEventListener("touchend", touchEnd);
-		obj.addEventListener("touchcancel", touchCancel);
+		cont_.addEventListener("touchstart", touchStart);
+		cont_.addEventListener("touchmove", touchMove);
+		cont_.addEventListener("touchend", touchEnd);
+		cont_.addEventListener("touchcancel", touchCancel);
 	}
 	else {
-		obj.addEventListener("mousedown", mouseDown);
+		cont_.addEventListener("mousedown", mouseDown);
 	}
+	cont_.style.overflow = "hidden";
 
 	function getPos(ev)
 	{
@@ -113,11 +126,14 @@ function initPullList(obj, opt)
 		}
 
 		if (ac == "U") {
-			msg = dy >= threshold? "松开加载~~~": "上拉加载...";
+			msg = dy >= threshold? "<b>松开加载~~~</b>": "即将加载...";
 		}
 		else if (ac == "D") {
-			msg = dy >= threshold? "松开刷新~~~": "下拉刷新...";
+			msg = dy >= threshold? "<b>松开刷新~~~</b>": "即将刷新...";
 		}
+		var maxY = threshold*1.2;
+		if (dy > maxY)
+			dy = maxY;
 
 		if (msg == null) {
 			jo.height(0).remove();
@@ -126,32 +142,32 @@ function initPullList(obj, opt)
 		jo.html(msg);
 		jo.height(dy).css("lineHeight", dy + "px");
 		if (ac == "D") {
-			jo.prependTo(obj);
+			jo.prependTo(cont_);
 		}
 		else if (ac == "U") {
-			jo.appendTo(obj);
+			jo.appendTo(cont_);
 		}
 	}
 
 	// ac为null时，应清除提示效果
 	function updateHint(ac, dy)
 	{
-		if (ac == null || dy == 0 || (opt.autoLoadMore && ac == 'U')) {
+		if (ac == null || dy == 0 || (opt_.autoLoadMore && ac == 'U')) {
 			ac = null;
 		}
 		else {
 			dy = Math.abs(dy);
 		}
-		opt.onHint.call(this, ac, dy, opt.threshold);
+		opt_.onHint.call(this, ac, dy, opt_.threshold);
 	}
 
 	function touchStart(ev)
 	{
 		var p = getPos(ev);
-		m_touchev = {
+		touchev_ = {
 			ac: null,
 			// 原始top位置
-			top0: obj.scrollTop,
+			top0: cont_.scrollTop,
 			// 原始光标位置
 			x0: p[0],
 			y0: p[1],
@@ -171,7 +187,7 @@ function initPullList(obj, opt)
 
 	function mouseDown(ev)
 	{
-		m_mouseMoved = false;
+		mouseMoved_ = false;
 		touchStart(ev);
 		// setCapture
 		window.addEventListener("mousemove", mouseMove, true);
@@ -183,7 +199,7 @@ function initPullList(obj, opt)
 	function click(ev)
 	{
 		window.removeEventListener("click", click, true);
-		if (m_mouseMoved)
+		if (mouseMoved_)
 		{
 			ev.stopPropagation();
 			ev.preventDefault();
@@ -192,12 +208,12 @@ function initPullList(obj, opt)
 
 	function mouseMove(ev)
 	{
-		if (m_touchev == null)
+		if (touchev_ == null)
 			return;
 
 		touchMove(ev);
-		if (m_touchev.dx != 0 || m_touchev.dy != 0)
-			m_mouseMoved = true;
+		if (touchev_.dx != 0 || touchev_.dy != 0)
+			mouseMoved_ = true;
 		ev.stopPropagation();
 		ev.preventDefault();
 	}
@@ -214,7 +230,7 @@ function initPullList(obj, opt)
 	function touchMove(ev)
 	{
 		var p = getPos(ev);
-		var m = m_touchev.momentum;
+		var m = touchev_.momentum;
 		if (m) {
 			var now = new Date();
 			if ( now - m.startTime > SAMPLE_INTERVAL ) {
@@ -224,36 +240,36 @@ function initPullList(obj, opt)
 			}
 		}
 
-		m_touchev.dx = p[0] - m_touchev.x0;
-		m_touchev.dy = p[1] - m_touchev.y0;
+		touchev_.dx = p[0] - touchev_.x0;
+		touchev_.dy = p[1] - touchev_.y0;
 
-		obj.scrollTop = m_touchev.top0 - m_touchev.dy;
-		var dy = m_touchev.dy + (obj.scrollTop - m_touchev.top0);
-		m_touchev.pully = dy;
+		cont_.scrollTop = touchev_.top0 - touchev_.dy;
+		var dy = touchev_.dy + (cont_.scrollTop - touchev_.top0);
+		touchev_.pully = dy;
 
-		if (obj.scrollTop <= 0 && dy > 0) {
-			m_touchev.ac = "D";
+		if (cont_.scrollTop <= 0 && dy > 0) {
+			touchev_.ac = "D";
 		}
-		else if (dy < 0 && obj.scrollTop >= obj.scrollHeight - obj.clientHeight) {
-			m_touchev.ac = "U";
+		else if (dy < 0 && cont_.scrollTop >= cont_.scrollHeight - cont_.clientHeight) {
+			touchev_.ac = "U";
 		}
-		updateHint(m_touchev.ac, dy);
+		updateHint(touchev_.ac, dy);
 		ev.preventDefault();
 	}
 
 	function touchCancel(ev)
 	{
-		m_touchev = null;
+		touchev_ = null;
 		updateHint(null, 0);
 	}
 
 	function momentumScroll(ev, onScrollEnd)
 	{
-		if (m_touchev == null || m_touchev.momentum == null)
+		if (touchev_ == null || touchev_.momentum == null)
 			return;
 
 		// 惯性滚动
-		var m = m_touchev.momentum;
+		var m = touchev_.momentum;
 		var dt = new Date();
 		var duration = dt - m.startTime;
 		if (duration > SAMPLE_INTERVAL) {
@@ -275,7 +291,7 @@ function initPullList(obj, opt)
 		function moveNext() 
 		{
 			// 用户有新的点击，则取消动画
-			if (m_touchev != null)
+			if (touchev_ != null)
 				return;
 
 			var dt1 = new Date();
@@ -287,9 +303,9 @@ function initPullList(obj, opt)
 			// 变加速运动
 			deceleration *= 1.1;
 
-			var top = obj.scrollTop;
-			obj.scrollTop = top - s;
-			if (v0 * dir > 0 && top != obj.scrollTop) {
+			var top = cont_.scrollTop;
+			cont_.scrollTop = top - s;
+			if (v0 * dir > 0 && top != cont_.scrollTop) {
 				window.requestAnimationFrame(moveNext);
 			}
 			else {
@@ -301,33 +317,33 @@ function initPullList(obj, opt)
 	function touchEnd(ev)
 	{
 		updateHint(null, 0);
-		if (m_touchev == null || m_touchev.ac == null || Math.abs(m_touchev.pully) < opt.threshold)
+		if (touchev_ == null || touchev_.ac == null || Math.abs(touchev_.pully) < opt_.threshold)
 		{
 			momentumScroll(ev, onScrollEnd);
-			m_touchev = null;
+			touchev_ = null;
 			return;
 		}
-		console.log(m_touchev);
-		doAction(m_touchev.ac);
-		m_touchev = null;
+		console.log(touchev_);
+		doAction(touchev_.ac);
+		touchev_ = null;
 
 		function doAction(ac)
 		{
 			// pulldown
 			if (ac == "D") {
 				console.log("refresh");
-				opt.onLoadItem.call(obj, true);
+				opt_.onLoadItem.call(cont_, true);
 			}
 			else if (ac == "U") {
 				console.log("loaditem");
-				opt.onLoadItem.call(obj, false);
+				opt_.onLoadItem.call(cont_, false);
 			}
 		}
 
 		function onScrollEnd()
 		{
-			if (opt.autoLoadMore) {
-				var distanceToBottom = obj.scrollHeight - obj.clientHeight - obj.scrollTop;
+			if (opt_.autoLoadMore) {
+				var distanceToBottom = cont_.scrollHeight - cont_.clientHeight - cont_.scrollTop;
 				if (distanceToBottom <= TRIGGER_AUTOLOAD) {
 					doAction("U");
 				}
@@ -339,12 +355,17 @@ function initPullList(obj, opt)
 /**
 @fn initNavbarAndList(jpage, opt)
 
-对一个导航栏(class="mui-navbar")加若干列表(class="p-list")的典型页面进行逻辑封装，包括以下功能：
+对一个导航栏(class="mui-navbar")加若干列表(class="p-list")的典型页面进行逻辑封装；也可以是若干button对应若干div-list区域，一次只显示一个区域；
+特别地，也可以是只有一个list，并没有button或navbar对应。
+
+它包括以下功能：
 
 1. 首次进入页面时加载默认列表
 2. 任一列表支持下拉刷新，上拉加载（自动管理刷新和分页）
 3. 点击导航栏自动切换列表，仅当首次显示列表时刷新数据
 4. 支持强制刷新所有列表的控制，一般定义在page接口中，如 PageOrders.refresh
+
+## 例：一个navbar与若干list的组合
 
 基本页面结构如下：
 
@@ -364,9 +385,10 @@ function initPullList(obj, opt)
 	</div>
 
 上面页面应注意：
+
 - navbar在header中，不随着滚动条移动而改变位置
-- 默认显示的list应加上active类，否则自动以第一个作为显示列表。
-- mui-navbar在点击一项时，会在被点击的<a>按钮上以及它对应的(mui-linkto标识)组件上添加class="active"
+- 默认要显示的list应加上active类，否则自动取第一个显示列表。
+- mui-navbar在点击一项时，会在对应的div组件（通过被点击的<a>按钮上mui-linkto属性指定链接到哪个div）添加class="active"。
 
 js调用逻辑示例：
 
@@ -388,7 +410,88 @@ js调用逻辑示例：
 		}
 	});
 
-框架基本原理是在合适的时机，自动调用类似这样的逻辑：
+由于指定了pageItf属性，当外部页面设置了 PageOrders.refresh = true后，再进入本页面，所有关联的列表会在展现时自动刷新。且PageOrders.refresh会被自动重置为false.
+
+## 例：若干button与若干list的组合(必须一一对应)，打开页面时只展现一个列表，点击相应按钮显示相应列表。
+
+如果没有用navbar组件，而是一组button对应一组列表，点一个button显示对应列表，也可以使用本函数。页面如下：
+
+	<div mui-initfn="initPageOrders" mui-script="orders.js">
+		<div class="hd">
+			<h2>订单列表</h2>
+		</div>
+
+		<div class="bd">
+			<div class="p-panelHd">待服务</div>
+			<div class="p-panel">
+				<div id="lst1" class="p-list active"></div>
+			</div>
+
+			<div class="p-panelHd">已完成</div>
+			<div class="p-panel">
+				<div id="lst2" class="p-list" style="display:none"></div>
+			</div>
+		</div>
+	</div>
+
+js调用逻辑示例：
+
+	jpage.find(".p-panel").height(500); // !!! 注意：必须为list container指定高度，否则无法出现下拉列表。一般根据页高自动计算。
+
+	initNavbarAndList(jpage, {
+		pageItf: PageOrders,
+		navRef: ".p-panelHd", // 点标题栏，显示相应列表区
+		listRef: ".p-panel .p-list", // 列表区
+		...
+	});
+
+注意：navRef与listRef中的组件数目一定要一一对应。除了使用选择器，也可以直接用jQuery对象为navRef和listRef赋值。
+
+## 例：只有一个list 的简单情况，也可以调用本函数简化分页处理
+
+仍考虑上例，假如那两个列表需要进入页面时就同时显示，那么可以分开一一设置如下：
+
+	jpage.find(".p-panel").height(500); // 一定要为容器设置高度
+
+	initNavbarAndList(jpage, {
+		pageItf: PageOrders,
+		navRef: "", // 置空，表示不需要button链接到表，下面listRef中的多表各自显示不相关。
+		listRef: ".p-panel .p-list", // 列表区
+		onGetQueryParam: function (jlst, callParam) {
+			callParam.ac = jlst.attr("ac");
+			var param = callParam.queryParam;
+			param.cond = jlst.attr("cond");
+		},
+		...
+	});
+
+上例中，listRef参数也可以直接使用jQuery对象赋值。
+navRef是否为空的区别是，如果非空，则表示listRef是一组互斥的列表，点击哪个button，就会设置哪个列表为active列表。当切到当前页时，只显示或刷新active列表。
+
+如果是只包含一个列表的简单页面：
+
+	<div mui-initfn="initPageOrders" mui-script="orders.js">
+		<div class="hd">
+			<h2>订单列表</h2>
+		</div>
+
+		<div class="bd">
+			<div class="p-list"></div>
+		</div>
+	</div>
+
+由于bd对象的高度已自动设置，要设置p-list对象支持上下拉加载，可以简单调用：
+
+	initNavbarAndList(jpage, {
+		pageItf: PageOrders,
+		navRef: "", // 一定置空，否则默认值是取mui-navbar
+		listRef: ".p-list"
+		...
+	});
+
+## 框架基本原理
+
+原理是在合适的时机，自动调用类似这样的逻辑：
 
 	var callParam = {ac: "Ordr.query", queryParam: {} };
 	opt.onGetQueryParam(jlst, callParam);
@@ -399,6 +502,8 @@ js调用逻辑示例：
 		if (data.d.length == 0)
 			opt.onNoItem(jlst);
 	});
+
+## 参数说明
 
 @param opt {onGetQueryParam, onAddItem, onNoItem?, pageItf?, navRef?=">.hd .mui-navbar", listRef=">.bd .p-list"}
 
@@ -421,17 +526,41 @@ js调用逻辑示例：
 
 在订单页面(PageOrder)修改订单后，如果想进入列表页面(PageOrders)时自动刷新所有列表，可以设置 PageOrders.refresh = true。
 设置opt.pageItf=PageOrders, 框架可自动检查和管理refresh变量。
+
+@param navRef,listRef  指定navbar与list，可以是选择器，也可以是jQuery对象；或是一组button与一组div，一次显示一个div；或是navRef为空，而listRef为一个或多个不相关联的list.
+
  */
 function initNavbarAndList(jpage, opt)
 {
-	var opt_ = opt;
-	var listRef = opt_.listRef || ">.bd .p-list";
-	var navRef = opt_.navRef || ">.hd .mui-navbar";
-	var jallList_ = jpage.find(listRef);
-	var jnav_ = jpage.find(navRef);
+	var opt_ = $.extend({
+		navRef: ">.bd .p-list",
+		listRef: ">.hd .mui-navbar"
+	}, opt);
+	var jallList_ = opt_.listRef instanceof jQuery? opt_.listRef: jpage.find(opt_.listRef);
+	var jbtns_ = opt_.navRef instanceof jQuery? opt_.navRef: jpage.find(opt_.navRef);
 	var firstShow_ = true;
 
+	if (jbtns_.hasClass("mui-navbar")) {
+		jbtns_ = jbtns_.find("a");
+	}
+	else {
+		linkNavbarAndList(jbtns_, jallList_);
+	}
+
 	init();
+
+	function linkNavbarAndList(jbtns, jlsts)
+	{
+		jbtns.each(function (i, e) {
+			$(e).data("linkTo", jlsts[i]);
+		});
+		jbtns.click(function () {
+			jlsts.removeClass("active");
+
+			var lst = $(this).data("linkTo");
+			$(lst).addClass("active");
+		});
+	}
 
 	function init()
 	{
@@ -450,7 +579,7 @@ function initNavbarAndList(jpage, opt)
 			}
 		}
 
-		jnav_.find("a").click(function (ev) {
+		jbtns_.click(function (ev) {
 			// 让系统先选中tab页再操作
 			setTimeout(function () {
 				showOrderList(false, true);
@@ -461,8 +590,11 @@ function initNavbarAndList(jpage, opt)
 			onLoadItem: showOrderList,
 			//onHint: $.noop
 		};
-		var container = jallList_[0].parentNode;
-		initPullList(container, pullListOpt);
+
+		jallList_.parent().each(function () {
+			var container = this;
+			initPullList(container, pullListOpt);
+		});
 
 		// 如果调用init时页面已经显示，则补充调用一次。
 		if (MUI.activePage && MUI.activePage.attr("id") == jpage.attr("id")) {
