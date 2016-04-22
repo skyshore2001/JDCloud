e2cc5f2eb647e75362140e7229467df0784f60da
liangjian <liangjian@oliveche.com>
2016-04-22 13:54:26 +0800

    pullList更新：滚动更顺滑（初速度提高，且使用变加速运动）；支持下拉到底自动加载更多数据。

--- product/server/m2/app.js
+++ product/server/m2/app.js
@@ -47,44 +47,43 @@
 - 由于page body的高度自动由框架设定，所以可以作为带滚动条的容器；如果是其它容器，一定要确保它有限定的宽度，以便可以必要时出现滚动条。
 
 @param obj 容器，它的高度应该是限定的，因而当内部内容过长时才可出现滚动条
-@param opt {onLoadItem, threshold?=200, maxMargin?=50, onHint?}
+@param opt {onLoadItem, autoLoadMore?=true, threshold?=100, onHint?}
 
 @param onLoadItem function(isRefresh)
 
 在合适的时机，它调用 onLoadItem(true) 来刷新列表，调用 onLoadItem(false) 来加载列表的下一页。在该回调中this为obj对象（即容器）。实现该函数时应当自行管理当前的页号(pagekey)
 
+@param autoLoadMore 当滑动到页面下方时（距离底部TRIGGER_AUTOLOAD=30px以内）自动加载更多项目。
+
 @param threshold 像素值。
 
 手指最少下划或上划这些像素后才会触发实际加载动作。
 
-@param onHint function(msg, value, ac)
+@param onHint function(ac, dy, threshold)
 
-	msg 提示信息，如"下拉刷新", "松开刷新"等
-	value 整数, 在[0,100]间, 用于显示进度条。
-	ac  动作。"D"表示下拉(down), "U"表示上拉(up).
+	ac  动作。"D"表示下拉(down), "U"表示上拉(up), 为null时应清除提示效果.
+	dy,threshold  用户移动偏移及临界值。dy>threshold时，认为触发加载动作。
 
-提供提示用户刷新或加载的动画效果. 缺省实现是下拉或上拉时显示提示信息。当msg为null时应去除提示效果。
+提供提示用户刷新或加载的动画效果. 缺省实现是下拉或上拉时显示提示信息。
 
-@param maxMargin 列表最大下拉或上拉位移
-
-与缺省的的提示动画配合使用，为列表最大可移动的距离。
 */
 function initPullList(obj, opt)
 {
 	var m_touchev = null; // {ac, x0, y0}
 	var m_mouseMoved = false;
-	var SAMPLE_INTERVAL = 300;
+	var SAMPLE_INTERVAL = 200; // ms
+	var TRIGGER_AUTOLOAD = 30; // px
 
 	window.requestAnimationFrame = window.requestAnimationFrame || function (fn) {
 		setTimeout(fn, 1000/60);
 	};
 
 	if (opt.threshold == null)
-		opt.threshold = 200;
-	if (opt.maxMargin == null)
-		opt.maxMargin = 50;
+		opt.threshold = 100;
 	if (opt.onHint == null)
 		opt.onHint = onHint;
+	if (opt.autoLoadMore == null)
+		opt.autoLoadMore = true;
 
 	if ("ontouchstart" in window) {
 		obj.addEventListener("touchstart", touchStart);
@@ -106,46 +105,44 @@ function initPullList(obj, opt)
 	}
 
 	var jo;
-	function onHint(msg, value, ac)
+	function onHint(ac, dy, threshold)
 	{
+		var msg = null;
 		if (jo == null) {
 			jo = $("<div style='overflow:hidden;text-align:center;vertical-align:middle'></div>");
 		}
+
+		if (ac == "U") {
+			msg = dy >= threshold? "松开加载~~~": "上拉加载...";
+		}
+		else if (ac == "D") {
+			msg = dy >= threshold? "松开刷新~~~": "下拉刷新...";
+		}
+
 		if (msg == null) {
 			jo.height(0).remove();
 			return;
 		}
 		jo.html(msg);
-		var value2 = value * opt.maxMargin / 100;
-		jo.height(value2).css("lineHeight", value2 + "px");
+		jo.height(dy).css("lineHeight", dy + "px");
 		if (ac == "D") {
 			jo.prependTo(obj);
 		}
 		else if (ac == "U") {
 			jo.appendTo(obj);
-			// 滚动到底
-			obj.scrollTop = obj.scrollHeight - obj.clientHeight;
 		}
 	}
 
+	// ac为null时，应清除提示效果
 	function updateHint(ac, dy)
 	{
-		var msg, value;
-		if (ac == null || dy == 0) {
-			msg = null;
-			value = 0;
+		if (ac == null || dy == 0 || (opt.autoLoadMore && ac == 'U')) {
+			ac = null;
 		}
 		else {
 			dy = Math.abs(dy);
-			value = Math.ceil(Math.min(dy *100 / opt.threshold, 100));
-			if (ac == "U") {
-				msg = value == 100? "松开加载...": "上拉加载...";
-			}
-			else if (ac == "D") {
-				msg = value == 100? "松开刷新...": "下拉刷新...";
-			}
 		}
-		opt.onHint.call(this, msg, value, ac);
+		opt.onHint.call(this, ac, dy, opt.threshold);
 	}
 
 	function touchStart(ev)
@@ -162,7 +159,7 @@ function initPullList(obj, opt)
 			dx: 0,
 			dy: 0,
 
-			// 用于惯性滚动: 每SAMPLE_INTERVAL(500ms)取样时最后一次时间及光标位置(用于计算初速度)
+			// 用于惯性滚动: 每SAMPLE_INTERVAL取样时最后一次时间及光标位置(用于计算初速度)
 			momentum: {
 				x0: p[0],
 				y0: p[1],
@@ -250,7 +247,7 @@ function initPullList(obj, opt)
 		updateHint(null, 0);
 	}
 
-	function momentumScroll(ev)
+	function momentumScroll(ev, onScrollEnd)
 	{
 		if (m_touchev == null || m_touchev.momentum == null)
 			return;
@@ -259,15 +256,20 @@ function initPullList(obj, opt)
 		var m = m_touchev.momentum;
 		var dt = new Date();
 		var duration = dt - m.startTime;
-		if (duration > SAMPLE_INTERVAL)
+		if (duration > SAMPLE_INTERVAL) {
+			onScrollEnd && onScrollEnd();
 			return;
+		}
 
 		var p = getPos(ev);
 		var v0 = (p[1]-m.y0) / duration;
-		if (v0 == 0)
+		if (v0 == 0) {
+			onScrollEnd && onScrollEnd();
 			return;
+		}
 
-		var deceleration = 0.0006;
+		v0 *= 2.5;
+		var deceleration = 0.0005;
 
 		window.requestAnimationFrame(moveNext);
 		function moveNext() 
@@ -282,11 +284,16 @@ function initPullList(obj, opt)
 			var s = v0 * t / 2;
 			var dir = v0<0? -1: 1;
 			v0 -= deceleration * t * dir;
+			// 变加速运动
+			deceleration *= 1.1;
 
 			var top = obj.scrollTop;
 			obj.scrollTop = top - s;
 			if (v0 * dir > 0 && top != obj.scrollTop) {
 				window.requestAnimationFrame(moveNext);
+			}
+			else {
+				onScrollEnd && onScrollEnd();
 			}
 		}
 	}
@@ -296,21 +303,36 @@ function initPullList(obj, opt)
 		updateHint(null, 0);
 		if (m_touchev == null || m_touchev.ac == null || Math.abs(m_touchev.pully) < opt.threshold)
 		{
-			momentumScroll(ev);
+			momentumScroll(ev, onScrollEnd);
 			m_touchev = null;
 			return;
 		}
 		console.log(m_touchev);
-		// pulldown
-		if (m_touchev.ac == "D") {
-			console.log("refresh");
-			opt.onLoadItem.call(obj, true);
-		}
-		else {
-			console.log("loaditem");
-			opt.onLoadItem.call(obj, false);
-		}
+		doAction(m_touchev.ac);
 		m_touchev = null;
+
+		function doAction(ac)
+		{
+			// pulldown
+			if (ac == "D") {
+				console.log("refresh");
+				opt.onLoadItem.call(obj, true);
+			}
+			else if (ac == "U") {
+				console.log("loaditem");
+				opt.onLoadItem.call(obj, false);
+			}
+		}
+
+		function onScrollEnd()
+		{
+			if (opt.autoLoadMore) {
+				var distanceToBottom = obj.scrollHeight - obj.clientHeight - obj.scrollTop;
+				if (distanceToBottom <= TRIGGER_AUTOLOAD) {
+					doAction("U");
+				}
+			}
+		}
 	}
 }
 
@@ -432,7 +454,6 @@ function initNavbarAndList(jpage, opt)
 		});
 
 		var pullListOpt = {
-			maxMargin: 50,
 			onLoadItem: showOrderList,
 			//onHint: $.noop
 		};
