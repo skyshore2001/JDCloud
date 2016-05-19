ace59c7f8d1ca7d5c4c437e8f33e8e0d93807d2e
liangjian <liangjian@oliveche.com>
2016-05-19 13:54:07 +0800

    add template for conf.user.php: config DB, DBCRED, etc.

---
+++ product/server/php/conf.user.template.php
@@ -0,0 +1,21 @@
+<?php
+
+if (getenv("P_DB") === false) {
+	// use SQLite:
+	//putenv("P_DB=myorder.db");
+	//putenv("P_DBCRED=");
+
+	// use MySQL:
+	putenv("P_DB=localhost/myorder");
+	putenv("P_DBCRED=ZGVtbzpkZW1vMTIz");
+
+	// Set the base URL, it affect session security, etc.
+	putenv("P_URL_PATH=/jdcloud");
+}
+
+// debug level: default value=0
+// putenv("P_DEBUG=9");
+
+// test mode: default value=0
+// $GLOBALS["TEST_MODE"] =1;
+
