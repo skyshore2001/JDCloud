e7a056a2fcafe16fbb4ed66785b580085cd8613a
liangjian <liangjian@oliveche.com>
2016-12-01 18:19:23 +0800

    删除webcc配置中不再使用的JQM框架相关部分

--- server/webcc.conf.php
+++ server/webcc.conf.php
@@ -11,8 +11,6 @@ $FILES = [
  */
 
 $RULES = [
-	'm/*.html' => 'HASH',
-	'm/lib/app_fw.js' => 'HASH',
 	'm2/*.html' => 'HASH',
 
 	'm/cordova/cordova.js' => '
@@ -20,21 +18,6 @@ git ls-files m/cordova | grep -i "\.js$" | xargs cat | jsmin > $TARGET',
 
 	'm/cordova-ios/cordova.js' => '
 git ls-files m/cordova-ios | grep -i "\.js$" | xargs cat | jsmin > $TARGET',
-
-	'm/lib-jqm.min.js' => '
-cd m/lib
-cat jquery.mobile-1.4.5.min.js \
-	jquery.validate.min.js \
-	messages_zh.min.js \
-	> $TARGET
-',
-
-	'm/lib-app.min.js' => ['
-cd m/lib
-cat common.js app_fw.js ../app.js | jsmin > $TARGET
-', 'HASH'],
-
-	'm/main.js' => 'jsmin < m/main.js > $TARGET',
 
 	'm2/lib-app.min.js' => ['
 cd m2
