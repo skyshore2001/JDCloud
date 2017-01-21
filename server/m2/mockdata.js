a003ddce985526e1f520bfd5980b27b4c71e7353
liangjian <liangjian@oliveche.com>
2017-01-21 17:37:36 +0800

    Squashed commits of master

--- server/m2/mockdata.js
+++ server/m2/mockdata.js
@@ -7,6 +7,7 @@
 	// id = index +100
 	// status=CR-created/RE-received/CA-cancelled
 
+	MUI.options.mockDelay = 500; // 模拟调用时间，毫秒
 	MUI.mockData = {
 		"login": [0, user],
 		"logout": [0, "OK"],
@@ -37,7 +38,17 @@
 			else {
 				arr = orders;
 			}
-			return [0, arr];
+			// 分页模拟, 倒序排列
+			var ret = {list: []};
+			var pagesz = param._pagesz || 20;
+			var pagekey = param._pagekey || 0;
+
+			for (var n=0, i=pagekey; n<pagesz && i<arr.length; ++n, ++i) {
+				ret.list.push(arr[arr.length-i-1]);
+			}
+			if (n == pagesz)
+				ret.nextkey = i;
+			return [0, ret];
 		},
 		"Ordr.set": function (param, postParam) {
 			var idx = param.id - 100;
@@ -48,5 +59,14 @@
 			$.extend(o, postParam);
 			return [0, "OK"];
 		}
+	}
+
+	// 模拟订单
+	var INIT_CNT = 43;
+	for (var i=0; i<INIT_CNT; ++i) {
+		MUI.mockData["Ordr.add"](null, {
+			dscr: i%4!=0? "基础套餐": "精英套餐",
+			amount: i%4!=0? 128: 228
+		});
 	}
 })();
