0baf367b3a706cbfab1d76c3d0b60e21db250924
liangjian <liangjian@oliveche.com>
2017-01-23 18:21:46 +0800

    Squashed commit of master

--- server/webcc.conf.php
+++ server/webcc.conf.php
@@ -14,11 +14,14 @@ $RULES = [
 	'm2/*.html' => 'HASH',
 	'm2/lib/app_fw.js' => 'HASH',
 
+# 如果支持App则使用这部分
 	'm2/cordova/cordova.js' => '
-git ls-files m2/cordova | grep -i "\.js$" | xargs cat | jsmin > $TARGET',
+cd m2/cordova
+sh -c "$WEBCC_LS_CMD" | grep -i "\.js$" | xargs cat | jsmin > $TARGET',
 
 	'm2/cordova-ios/cordova.js' => '
-git ls-files m2/cordova-ios | grep -i "\.js$" | xargs cat | jsmin > $TARGET',
+cd m2/cordova-ios
+sh -c "$WEBCC_LS_CMD" | grep -i "\.js$" | xargs cat | jsmin > $TARGET',
 
 
 ];
