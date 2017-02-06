3f52b0bfeaa62f0ef8ec473f11ea948b260f0b91
liangjian <liangjian@oliveche.com>
2017-02-06 09:47:59 +0800

    从筋斗云框架中剥离后端框架部分

--- server/m2/page/home.js
+++
@@ -1,4 +0,0 @@
-function initPageHome()
-{
-	var jpage = $(this);
-}
--- server/m2/page/login1.js
+++
@@ -1,7 +0,0 @@
-function initPageLogin1()
-{
-	var jpage = $(this);
-
-	var jf = jpage.find("form");
-	MUI.setFormSubmit(jf, handleLogin);
-}
