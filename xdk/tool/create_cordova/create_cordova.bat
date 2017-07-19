b79d459e62a7b81e1311e1d0523b1b76871f6812
liangjian <liangjian@oliveche.com>
2017-07-19 15:30:46 +0800

    create ios/android app with config

--- xdk/tool/create_cordova/create_cordova.bat
+++ xdk/tool/create_cordova/create_cordova.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion enableextensions
 
-set src=client
-set dst=client-test
+set src=jdcloud
+set dst=jdcloud-app
 set appId=com.daca.jdcloud
 set appName=jdcloud
 
@@ -11,9 +11,16 @@ call cordova create %dst% %appId% %appName%
 cd %dst%
 mv www www1
 cp -r ../%src%/www ./
+mv www/icon ./
+mv config.xml config.xml.bak
+cp ../%src%/config.xml ./
 call cordova platform add android 
 
+call cordova plugin add ../%src%/plugins/cordova-plugin-statusbar
+call cordova plugin add ../%src%/plugins/cordova-plugin-device
 call cordova plugin add ../%src%/plugins/cordova-plugin-splashscreen
-call cordova plugin add ../%src%/plugins/org.apache.cordova.camera
-call cordova plugin add ../%src%/plugins/com.justep.cordova.plugin.weixin.v3 --variable WEIXIN_APPID=wx0136112850b90306 --variable WEIXIN_PARTNER_ID=1263699901 --variable WEIXIN_API_KEY=uOSaobd6z0wnKVH2fOSl251YUqcK6160
+call cordova plugin add ../%src%/plugins/cordova-plugin-camera
+call cordova plugin add ../%src%/plugins/cordova-plugin-geolocation
+
+::call cordova plugin add ../%src%/plugins/com.justep.cordova.plugin.weixin.v3 --variable WEIXIN_APPID=wx0136112850b90XXX --variable WEIXIN_PARTNER_ID=1263699XXX --variable WEIXIN_API_KEY=uOSaobd6z0wnKVH2fOSl251YUqcK6XXX
 
