c609763a89a9d8aec9f258228d94dce4d5a848de
skyshore <skyshore@gmail.com>
2018-10-30 21:10:00 +0800

    merge from coupon: update pageTpl.js

--- tool/create-wui-page/template/pageTpl.js
+++ tool/create-wui-page/template/pageTpl.js
@@ -4,8 +4,17 @@ function initPage<?=$obj?>()
 	var jtbl = jpage.find("#tbl<?=$obj?>");
 	var jdlg = $("#dlg<?=$obj?>");
 
+/*
 	// 定制工具栏增删改查按钮：r(refresh), f(find), a(add), s(set), d(del)
-	// jtbl.jdata().toolbar = "rfs";
+	jtbl.jdata().toolbar = "rfs";
+	// 自定义按钮
+	var btn1 = {text: "结算明细", iconCls:'icon-ok', handler: function () {
+		var row = WUI.getRow(jtbl);
+		if (row == null)
+			return;
+		WUI.showPage("pageOrder", "结算明细-" + row.id + "-" + row.startDt, [ {cond: "closeLogId="+row.id} ]);
+	}};
+*/
 
 	jtbl.datagrid({
 		url: WUI.makeUrl("<?=$baseObj?>.query"),
