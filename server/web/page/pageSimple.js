60dec7349b853ba2b6f3a4ec79353ba26ce90843
liangjian <liangjian@lj-m>
2020-06-27 20:31:41 +0800

    merge from demo2: pageSimple

--- server/web/page/pageSimple.js
+++ server/web/page/pageSimple.js
@@ -3,7 +3,10 @@ function initPageSimple(url, queryParams)
 	var jpage = $(this);
 	var jtbl = jpage.find("table:first");
 
+	WUI.assert(url.makeUrl, "url须由makeUrl生成");
 	jtbl.jdata().toolbar = "r";
+	// 这里未直接用"export"而是重新定义，是为了禁止WUI.getExportHandler自动生成res参数。
+	var btnExport = {text:'导出', iconCls:'icon-save', handler: WUI.getExportHandler(jtbl, null, {res: url.params.res || null}) };
 
 	var url1 = WUI.makeUrl(url, queryParams);
 	callSvr(url1, function (data) {
@@ -13,7 +16,7 @@ function initPageSimple(url, queryParams)
 		jtbl.datagrid({
 			columns: [ columns ],
 			data: data,
-			toolbar: WUI.dg_toolbar(jtbl, null, "export")
+			toolbar: WUI.dg_toolbar(jtbl, null, btnExport)
 		});
 		var opt = jtbl.datagrid("options");
 		opt.url = url;
