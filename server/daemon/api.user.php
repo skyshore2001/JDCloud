388b1e1ffc131f703507eb43e4348ad77a64489b
liangjian <liangjian@yibo>
2022-04-07 03:50:51 +0800

    bugfix: daemon example

--- server/daemon/api.user.php
+++ server/daemon/api.user.php
@@ -3,14 +3,14 @@
 function api_hello($env)
 {
 	addLog([
-		"get" => $env->_GET(),
-		"post" => $env->_POST(),
+		"get" => $env->_GET,
+		"post" => $env->_POST,
 		"header" => $env->header(),
-		"session" => $env->_SESSION(),
+		"session" => $env->_SESSION,
 	]);
 	// return ["id"=>100, "hello"=>"world"];
 	$id = $env->param("id");
-	$hello = $env->queryOne("SELECT * FROM ApiLog ORDER BY id DESC LIMIT 1");
+	$hello = $env->queryOne("SELECT * FROM ApiLog ORDER BY id DESC LIMIT 1", true);
 
 	$cnt = ++ $env->_SESSION["cnt"];
 	return ["id"=>$id, "hello"=>$hello, "cnt"=>$cnt];
