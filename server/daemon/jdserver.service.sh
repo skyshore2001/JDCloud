3c3ae3d75f591fcac9e5b94d273818df5c392a6d
liangjian <liangjian@oliveche.com>
2022-04-28 11:49:03 +0800

    merge from vmstat: 服务管理命令增强，支持remove/start/stop/status等；bugfix: SIGPIPE被systemd忽略

--- server/daemon/jdserver.service.sh
+++ server/daemon/jdserver.service.sh
@@ -1,10 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+
+# 须在当前目录下运行
+# `sudo xx.service.sh`安装服务；
+# `sudo xx.service.sh remove`删除服务。
+# `sudo xx.service.sh start|stop|restart|status`启停或查看状态。
+# 服务命令会在当前目录下执行。
 
 svc=`basename $0 .service.sh`
 dir=`realpath .`
 if [[ $UID != 0 ]]; then
 	echo "*** Error: use root user or sudo"
 	exit 1
+fi
+
+if [[ $1 == remove ]]; then
+	systemctl stop $svc 2>/dev/null
+	rm /usr/lib/systemd/system/$svc.service 2>/dev/null && echo "=== service $svc removed"
+	exit
+elif [[ -n $1 ]]; then
+	systemctl $1 $svc
+	exit
 fi
 
 cat <<. > /usr/lib/systemd/system/$svc.service
@@ -19,6 +34,9 @@ ExecStart=/bin/sh -c "swoole $svc.php >> $svc.log 2>&1"
 WorkingDirectory=$dir
 Restart=on-failure
 RestartSec=5s
+
+# 避免systemd中SIGPIPE信号被忽略
+IgnoreSIGPIPE=no
 
 [Install]
 WantedBy=multi-user.target
