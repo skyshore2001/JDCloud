eaec25da540686a0b753d2c8bda87bfd8c9bc9e9
liangjian <liangjian@oliveche.com>
2023-04-24 14:32:41 +0800

    merge from firefly-web: tool/cmdtool例子更新

--- tool/cmdtool.template.php
+++ tool/cmdtool.template.php
@@ -1,18 +1,37 @@
 <?php
 // run setenv to choose db
 
-chdir(dirname(__FILE__));
+chdir(__DIR__);
 
-require_once("../server/app.php");
+require_once("../server/api.php");
 
 $GLOBALS["dbConfirmFn"] = function ($connstr) {
 	echo "=== connect to $connstr (enter to cont, ctrl-c to break) ";
 	fgets(STDIN);
 };
 
+//$env = new DBEnv("mysql", "mysql:host=localhost;port=3306;dbname=firefly_web", "root", "123456");
+$env = getJDEnv();
+
 // example: update all names of users, e.g. "用户13712345678"->"用户137****5678"
-$rows = queryAll("select id, name, phone from User");
-global $DBH;
+$rows = $env->queryAll("select id, name, phone from User");
+
+$n = 0;
+foreach ($rows as $row) {
+	$id = $row[0];
+	$name = $row[1] ?: "User " . $row[2];
+	$name1 = preg_replace('/\d{3}\K\d{4}/', '****', $name);
+	if ($name1 != $name)
+	{
+		echo "update `{$name}` to `{$name1}`\n";
+		$env->dbUpdate("User", ["name"=>$name1], $id);
+		++ $n;
+	}
+}
+echo "update $n records.\n";
+
+/* example: use transaction and prepare
+$DBH = $env->DBH;
 $DBH->beginTransaction();
 $sth = $DBH->prepare("update User set name=? where id=?");
 $n = 0;
@@ -29,3 +48,4 @@ foreach ($rows as $row) {
 }
 $DBH->commit();
 echo "update $n records.\n";
+ */
