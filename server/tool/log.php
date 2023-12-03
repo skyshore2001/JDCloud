f2f87d6141daddb1f537a81daea6eb3a5749980e
liangjian <liangjian@dev6>
2023-12-03 21:11:30 +0800

    日志log.php支持下一页或指定页码

--- server/tool/log.php
+++ server/tool/log.php
@@ -4,8 +4,10 @@
 
 参数:
 
-- f: 指定显示哪个日志, 路径是相对于server目录下的位置, 不包含.log后缀
+- f: 指定显示哪个日志, 路径是相对于server目录下的位置, 不包含.log后缀；
+如果再指定参数f1=1，则读备份文件如trace.log.1
 - sz: 指定一次取多少字节显示
+- page: 指定页码，默认为-1，即最后一页，下一页则为page=-2；也可以为1, 2等正数，表示从头开始读。注意日志显示时仍为倒序。
 - noHeader: 设置为1时不显示标题.用于直接集成在系统中.
 
 支持日志格式：以"=== ... at [时间] "或"=== [时间] "开头，标识出一项记录。
@@ -17,11 +19,17 @@ include_once("../php/conf.user.php");
 
 $F0 = @$_GET["f"] ?: "trace";
 $F = (@$GLOBALS["conf_dataDir"] ?: "..") . "/{$F0}.log";
-$MAX_READ_SZ = @intval($_GET["sz"])?:50000;
+$PAGE_SZ = @intval($_GET["sz"])?:50000;
+$PAGE = @intval($_GET["page"])?:-1;
+
+// 读备份文件 xx.log.1
+if (@$_GET["f1"]) {
+	$F .= ".1";
+}
 
 $msgs = [];
 
-function loadMsgs($f, $readSz)
+function loadMsgs($f, $pagesz, $page = -1)
 {
 	global $msgs;
 
@@ -30,13 +38,27 @@ function loadMsgs($f, $readSz)
 		return 0;
 
 	$fp = fopen($f, "r");
-	if ($sz > $readSz) {
-		fseek($fp, -$readSz, SEEK_END);
+	$pos = 0;
+	if ($page > 0) {
+		$pos = ($page-1) * $pagesz;
+		if ($pos > $sz)
+			return 0;
 	}
+	else if ($page < 0) {
+		$pos = $sz - (-$page * $pagesz);
+		if ($pos + $pagesz < 0)
+			return 0;
+		if ($pos < 0) {
+			$pos = 0;
+		}
+	}
+	fseek($fp, $pos, SEEK_SET);
 
 	$curMsg = null;
 	while ( ($s = fgets($fp)) !== false) {
 		if (preg_match('/^===(.{1,100} at)? \[/', $s)) {
+			if ($page != -1 && ftell($fp) - $pos > $pagesz)
+				break;
 			if ($curMsg != null)
 				$msgs[] = $curMsg;
 			$curMsg = $s;
@@ -49,13 +71,14 @@ function loadMsgs($f, $readSz)
 		$msgs[] = $curMsg;
 
 	fclose($fp);
-	return min($sz, $readSz);
+	return min($pagesz, $sz);
 }
 
-$rv = loadMsgs($F, $MAX_READ_SZ);
-if ($rv > 0 && $rv < $MAX_READ_SZ) {
-	loadMsgs($F .".1", $MAX_READ_SZ - $rv);
+$rv = loadMsgs($F, $PAGE_SZ, $PAGE);
+if ($PAGE == -1 && $rv > 0 && $rv < $PAGE_SZ) {
+	loadMsgs($F .".1", $PAGE_SZ - $rv);
 }
+$showNext = ($rv >= $PAGE_SZ);
 ?>
 <!DOCTYPE html>
 <html>
@@ -73,11 +96,11 @@ xmp {
 
 <?php if (! @$_GET["noHeader"]) { ?>
 <h2>查看日志</h2>
-<a href="?f=trace&sz=<?=$MAX_READ_SZ?>">trace</a>
-<a href="?f=ext&sz=<?=$MAX_READ_SZ?>">ext</a>
-<a href="?f=debug&sz=<?=$MAX_READ_SZ?>">debug</a>
-<a href="?f=slow&sz=<?=$MAX_READ_SZ?>">slow</a>
-<a href="?f=daemon/jdserver&sz=<?=$MAX_READ_SZ?>">jdserver</a>
+<a href="?f=trace&sz=<?=$PAGE_SZ?>">trace</a>
+<a href="?f=ext&sz=<?=$PAGE_SZ?>">ext</a>
+<a href="?f=debug&sz=<?=$PAGE_SZ?>">debug</a>
+<a href="?f=slow&sz=<?=$PAGE_SZ?>">slow</a>
+<a href="?f=daemon/jdserver&sz=<?=$PAGE_SZ?>">jdserver</a>
 
 <hr/>
 <?php
@@ -101,6 +124,12 @@ else {
 
 		echo "<xmp>" . $msg . "</xmp><hr/>\n";
 	}
+}
+
+if ($showNext) {
+	$_GET["page"] = $PAGE>0? ($PAGE+1): ($PAGE-1);
+	$p = http_build_query($_GET);
+	echo("<a href=\"?$p\">下一页</a>");
 }
 ?>
 </body>
